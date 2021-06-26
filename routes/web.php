0b3122d7c58f36699dce861d14d0409dd1fe44c0
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2021-06-26 18:35:37 +0700

    Add view blade route function for debugging purpose.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/debug')->group(function(){
+    Route::get('/view/{view}', function($view){
+        return view($view);
+    });
+});
