dcec917e9cb4fed53c4016b678603ebeaf8c20b1
BankruptJere <jeremiahhartanto17@gmail.com>
2021-08-03 19:16:53 +0700

    Test Commit

---
+++ html/Modal.js
@@ -0,0 +1,10 @@
+document.getElementById('buttonz').addEventListener('click',
+function() {
+        document.querySelector('.bg-modalz').getElementsByClassName.display = 'flex';
+    });
+
+    document.getElementById('close-buttonz').addEventListener('click',
+    function() {
+            document.querySelector('.bg-modalz').getElementsByClassName.display = 'none';
+            
+        });
